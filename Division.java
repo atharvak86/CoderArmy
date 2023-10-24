1ebc9403838f8a97820db8c5ad34708cc9fdef4f
ATHARVA DINESH KHADSE <84701286+atharvak86@users.noreply.github.com>
2023-10-24 13:21:52 +0530

    Add files via upload

---
+++ Division.java
@@ -0,0 +1,15 @@
+public class Division 
+{
+    public static float calDivision(float num1, float num2)
+    {
+        return num1/num2;
+    }
+    public static void main(String[] args) 
+    {
+        float a = 12;
+        float b = 6;
+
+        float div = calDivision(a, b);
+        System.out.println(div);
+    }    
+}
